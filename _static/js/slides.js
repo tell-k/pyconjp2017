af5bd440c64cc12f79ee8c85c0df668e35ce9dd5
tell-k <ffk2005@gmail.com>
2017-10-30 08:39:55 +0900

    commit

--- _static/js/slides.js
+++ _static/js/slides.js
@@ -460,7 +460,7 @@ function addGeneralStyle() {
 
   var el = document.createElement('meta');
   el.name = 'viewport';
-  el.content = 'width=768,height=523';
+  el.content = 'width=1100,height=750';
   document.querySelector('head').appendChild(el);
 
   var el = document.createElement('meta');
